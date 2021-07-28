11fa057d8af2a76755f66a02cbc9dbaae36ba5a7
ajay <ajaydiwakarnl>
2021-07-28 19:05:36 +0530

    change

---
+++ resources/js/components/ChatDefaultComponent.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "ChatDefaultComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/ChatPanelheadingComponent.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ChatPanelheadingComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
